1997552f83cddd56d3ead7ef24264034582a9605
Regan Tan <regantanyk@gmail.com>
2025-04-15 10:48:17 +0800

    Fix: force Node 18 and clean reinstall for Render

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,25 +2,27 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
 import path from "path";
 
 import { connectDB } from "./lib/db.js";
-
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
 
 dotenv.config();
-
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5001;
 const __dirname = path.resolve();
+
+const allowedOrigins = ["http://localhost:5173"];
+if (process.env.NODE_ENV === "production") {
+  allowedOrigins.push("https://real-time-chat-webapp-using-react-and-socket-io.vercel.app/");
+}
 
 app.use(express.json({ limit: "30mb" }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: allowedOrigins,
     credentials: true,
   })
 );
@@ -30,9 +32,8 @@ app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
 
