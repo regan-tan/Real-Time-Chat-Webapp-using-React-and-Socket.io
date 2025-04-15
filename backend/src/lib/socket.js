4389e7a9e69f975c47745e7b220293969ce8d2da
Regan Tan <regantanyk@gmail.com>
2025-04-15 12:44:21 +0800

    updated origin for prod in cors socket.js

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,7 +7,10 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://real-time-chat-webapp-using-react-and-socket-io.vercel.app", // production frontend
+    ],
   },
 });
 
